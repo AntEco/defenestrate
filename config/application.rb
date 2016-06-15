f768aa9721523551795bae93c87e6beb66fbd3f5
Anton Economos <anton.economos@gmail.com>
2016-06-15 14:12:17 -0400

    trying to make game embeddable

--- config/application.rb
+++ config/application.rb
@@ -8,6 +8,10 @@ Bundler.require(*Rails.groups)
 
 module Defenestrate
   class Application < Rails::Application
+
+    config.action_dispatch.default_headers = {
+    'X-Frame-Options' => 'ALLOWALL'
+    }
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
